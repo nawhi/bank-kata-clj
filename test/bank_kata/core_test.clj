b7b89504c5beaa6caa29c9de04b113fa958858e9
nawhi <25394965+nawhi@users.noreply.github.com>
2021-09-20 15:54:01 +0100

    Use (str) instead of custom (big-string)

--- test/bank_kata/core_test.clj
+++ test/bank_kata/core_test.clj
@@ -6,18 +6,16 @@
 
 (defn date [iso] (LocalDate/parse iso))
 
-(defn big-string [& strings] (clojure.string/join "\n" strings))
-
 (deftest bank-kata-tests
   (testing "Acceptance Test"
     (let [account (create-account)]
       (do (deposit! account 1000 (date "2021-08-01"))
           (deposit! account 2000 (date "2021-08-03"))
           (withdraw! account 500 (date "2021-08-07")))
-      (let [expected (big-string "| Date       | Credit  | Debit   | Balance |"
-                                 "| 07/08/2021 |         |  500.00 | 2500.00 |"
-                                 "| 03/08/2021 | 2000.00 |         | 3000.00 |"
-                                 "| 01/08/2021 | 1000.00 |         | 1000.00 |")
+      (let [expected (str "| Date       | Credit  | Debit   | Balance |\n"
+                          "| 07/08/2021 |         |  500.00 | 2500.00 |\n"
+                          "| 03/08/2021 | 2000.00 |         | 3000.00 |\n"
+                          "| 01/08/2021 | 1000.00 |         | 1000.00 |")
             actual (statement account)]
         (is (= expected actual)))))
 
@@ -39,28 +37,32 @@
             (is (= expected @account))))))
 
   (testing "stringify-tx"
-    (is (=
-          "| 07/08/2021 |  500.00 |         | 1250.00 |"
-          (stringify-tx {:value 500 :date (date "2021-08-07")} 1250)))
-    (is (=
-          "| 08/09/2021 |         | 1200.00 | 1000.00 |"
-          (stringify-tx {:value -1200 :date (date "2021-09-08")} 1000))))
+    (is (= "| 07/08/2021 |  500.00 |         | 1250.00 |"
+           (stringify-tx {:value 500 :date (date "2021-08-07")} 1250)))
+
+    (is (= "| 08/09/2021 |         | 1200.00 | 1000.00 |"
+           (stringify-tx {:value -1200 :date (date "2021-09-08")} 1000))))
 
   (testing "Statements"
     (let [account (atom {:transactions [{:value 500 :date (date "2021-08-01")}]})]
-      (is (= (big-string "| Date       | Credit  | Debit   | Balance |"
-                         "| 01/08/2021 |  500.00 |         |  500.00 |")
+      (is (= (str "| Date       | Credit  | Debit   | Balance |\n"
+                  "| 01/08/2021 |  500.00 |         |  500.00 |")
              (statement account))))
+
     (let [account (atom {:transactions [{:value 500 :date (date "2021-08-01")}
                                         {:value -125 :date (date "2021-08-03")}]})]
-      (is (= (big-string "| Date       | Credit  | Debit   | Balance |"
-                         "| 03/08/2021 |         |  125.00 |  375.00 |"
-                         "| 01/08/2021 |  500.00 |         |  500.00 |")
+      (is (= (str "| Date       | Credit  | Debit   | Balance |\n"
+                  "| 03/08/2021 |         |  125.00 |  375.00 |\n"
+                  "| 01/08/2021 |  500.00 |         |  500.00 |")
              (statement account)))))
 
+  (testing "Handles floating points"
+    (let [account (create-account)]
+      (do (deposit! account 123.33 (date "2021-08-01"))
+          (withdraw! account 123.33 (date "2021-08-02"))
+          (let [expected {:transactions [{:value 123.33 :date (date "2021-08-01")}
+                                         {:value -123.33 :date (date "2021-08-02")}]}]
+            (is (= expected @account)))))))
 
-  ;(testing "TODO works with no rounding errors")
-  ;)
-  )
 
 
